b4a5d3dd1bc8fea0b00631bad9a8309fa05ef7c7
higor <higor.2120478300029@faeterj-rio.edu.br>
2024-06-18 10:07:47 -0300

    parte back end

---
+++ src/main/java/dev/higordevjs/movies/MoviesApplication.java
@@ -0,0 +1,18 @@
+package dev.higordevjs.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+
+
+}
